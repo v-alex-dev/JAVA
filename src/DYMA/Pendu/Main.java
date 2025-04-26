850038f056241256d9ba041d29a2996dc0194f5f
dev_vens <vensalex1991@gmail.com>
2025-04-26 14:53:50 +0200

    first commit

---
+++ src/DYMA/Pendu/Main.java
@@ -0,0 +1,12 @@
+package DYMA.Pendu;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static  void main (String[] args) {
+        var scanner = new Scanner(System.in);
+        var random = new Random();
+
+    }
+}
